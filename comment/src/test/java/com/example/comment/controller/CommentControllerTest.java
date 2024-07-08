539f1c9c64143a0d61950950fcd003af124fb660
Hoon <hoon0661@gmail.com>
2024-07-08 19:48:52 -0400

    Comment Service - added test cases for controller

---
+++ comment/src/test/java/com/example/comment/controller/CommentControllerTest.java
@@ -0,0 +1,80 @@
+package com.example.comment.controller;
+
+import com.example.comment.dto.CommentDto;
+import com.example.comment.request.CommentRequest;
+import com.example.comment.service.CommentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CommentService commentService;
+
+    private String testToken;
+
+    @BeforeEach
+    void setup() {
+        this.testToken = "AABB";
+    }
+
+    @Test
+    void create_comment() throws Exception {
+        Mockito.when(this.commentService.create(anyInt(), anyString(), anyString()))
+                        .thenReturn(any(CommentDto.class));
+        mockMvc.perform(
+                        post("/api/v1/comments")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsBytes(new CommentRequest("comment")))
+                                .header(HttpHeaders.AUTHORIZATION, this.testToken)
+
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void get_comments_by_postId() throws Exception {
+        Integer postId = 1;
+
+        Pageable pageable = mock(Pageable.class);
+        Mockito.when(this.commentService.findAllByPostId(postId, pageable, this.testToken))
+                .thenReturn(Page.empty());
+        mockMvc.perform(
+                        get("/api/v1/comments")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsBytes(new CommentRequest("comment")))
+                                .header(HttpHeaders.AUTHORIZATION, this.testToken)
+
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
