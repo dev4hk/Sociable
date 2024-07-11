e0436ee63a9b241890f55764d8bacc922e6e5b21
Hoon <hoon0661@gmail.com>
2024-07-11 12:39:48 -0400

    Frontend - added user search ui

--- frontend/src/components/search/UserSearch.tsx
+++ frontend/src/components/search/UserSearch.tsx
@@ -1,18 +1,24 @@
 import { Avatar, Card, CardHeader } from "@mui/material";
+import { useState } from "react";
 
 const users = [1, 1, 1, 1, 1];
 
 const UserSearch = () => {
+  const [username, setUsername] = useState("");
+  const handleSearchUser = (event: any) => {
+    setUsername(event.target.value);
+  };
   return (
     <div>
       <div className="py-5 relative">
         <input
           type="text"
-          className="bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full"
+          className="bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full text-white"
           placeholder="Search User..."
+          onKeyDown={handleSearchUser}
         />
         <div className="absolute w-full z-10 top-[4.5rem]">
-          {false &&
+          {username &&
             users.map((user, index) => (
               <Card key={index} className="cursor-pointer hover:text-sky-400">
                 <CardHeader
